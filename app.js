73dfca6454b27b9343cf76072a155fece13a8768
Duy Pham <duypham93@gmail.com>
2018-04-24 22:42:15 -0600

    Created Signin

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var mongoose = require('mongoose');
 var session = require('express-session');
 var passport = require('passport');
 var flash = require('connect-flash');
+var validator = require('express-validator');
 
 mongoose.connect(process.env.DB);
 require('./config/passport');
@@ -23,6 +24,7 @@ app.set('view engine', '.hbs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(validator());
 app.use(cookieParser());
 app.use(session({secret: process.env.SECRET, resave: false, saveUninitialized: false}));
 app.use(flash());
--- config/passport.js
+++ config/passport.js
@@ -15,6 +15,16 @@ passport.deserializeUser(function (id, done) {
 passport.use('local.signup', new localStrategy(
     {usernameField: 'email', passwordField: 'password', passReqToCallback: true},
     function(req, email, password, done) {
+        req.checkBody('email', 'Invalid email').notEmpty().isEmail();
+        req.checkBody('password', 'Password was too short!').notEmpty().isLength({min:4});
+        var errors = req.validationErrors();
+        if(errors){
+            var messages = [];
+            errors.forEach(function (error) {
+                messages.push(error.msg);
+            });
+            return done(null, false, req.flash('error', messages));
+        }
         User.findOne({'email': email}, function (err, user) {
             if(err){
                 return done(err);
@@ -31,6 +41,34 @@ passport.use('local.signup', new localStrategy(
                 }
                 return done(null, newUser);
             })
+        });
+    }
+));
+
+passport.use('local.signin', new localStrategy(
+    {usernameField: 'email', passwordField: 'password', passReqToCallback: true},
+    function(req, email, password, done) {
+        req.checkBody('email', 'Invalid email').notEmpty().isEmail();
+        req.checkBody('password', 'Password was too short!').notEmpty();
+        var errors = req.validationErrors();
+        if(errors){
+            var messages = [];
+            errors.forEach(function (error) {
+                messages.push(error.msg);
+            });
+            return done(null, false, req.flash('error', messages));
+        }
+        User.findOne({'email': email}, function (err, user) {
+            if(err){
+                return done(err);
+            }
+            if(!user){
+                return done(null, false, {message: 'No user found.'});
+            }
+            if(!user.validPassword(password)){
+                return done(null, false, {message: 'Wrong password.'});
+            }
+            return done(null, user);
         });
     }
 ));
